94cce5d43dfe6446287cd53ff70c4877ca96fae1
seohyoseok0408 <seohyoseok0408@gmail.com>
2024-07-11 08:18:50 +0900

    서블릿/jsp-detail컨트롤러 간단화

--- servlet-jsp/src/main/java/com/newlecture/web/controller/NoticeDetailController.java
+++ servlet-jsp/src/main/java/com/newlecture/web/controller/NoticeDetailController.java
@@ -1,6 +1,7 @@
 package com.newlecture.web.controller;
 
 import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,56 +17,9 @@ public class NoticeDetailController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id = Integer.parseInt(request.getParameter("id"));
 
-        String url = "jdbc:oracle:thin:@192.168.219.106:1521/ORCLPDB";
-        String sql = "SELECT * FROM NOTICE WHERE ID=?";
-
-
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            Connection con = DriverManager.getConnection(url, "NEWLEC", "newlec");
-            PreparedStatement st = con.prepareStatement(sql); // 미리 준비
-            st.setInt(1, id);
-            ResultSet rs = st.executeQuery();
-
-            rs.next();
-
-            String title = rs.getString("TITLE");
-            String writerId = rs.getString("WRITER_ID");
-            Date regdate = rs.getDate("REGDATE");
-            int hit = rs.getInt("HIT");
-            String files = rs.getString("FILES");
-            String content = rs.getString("CONTENT");
-
-            Notice notice = new Notice(
-                    id,
-                    title,
-                    writerId,
-                    regdate,
-                    hit,
-                    files,
-                    content
-            );
-            request.setAttribute("n", notice);
-            // detail.jsp 에서 ${n.title} 식으로 사용할 때, n.title의 getter 메소드인 n.getTitle() 메소드를 호출하는 코드로 변환
-
-            /*
-            request.setAttribute("title", title);
-            request.setAttribute("writerId", writerId);
-            request.setAttribute("regdate", regdate);
-            request.setAttribute("hit", hit);
-            request.setAttribute("files", files);
-            request.setAttribute("content", content);
-            */
-            rs.close();
-            st.close();
-            con.close();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        NoticeService service = new NoticeService();
+        Notice notice = service.getNotice(id);
+        request.setAttribute("n", notice);
 
         request.getRequestDispatcher("/WEB-INF/view/notice/detail.jsp").forward(request, response);
 
