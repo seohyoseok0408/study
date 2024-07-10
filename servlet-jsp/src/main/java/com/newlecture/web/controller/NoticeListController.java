1ad5afdb0b35f86da4befa587dc52d4f6996cc36
seohyoseok0408 <seohyoseok0408@gmail.com>
2024-07-10 17:08:01 +0900

    서블릿/jsp-검색기능

--- servlet-jsp/src/main/java/com/newlecture/web/controller/NoticeListController.java
+++ servlet-jsp/src/main/java/com/newlecture/web/controller/NoticeListController.java
@@ -17,8 +17,23 @@ import java.util.List;
 public class NoticeListController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //list?f=title&q=a
+
+
+
+        String field_ = request.getParameter("f");
+        String query_ = request.getParameter("q");
+
+        String field = "title";
+        if(field_ != null)
+            field = field_;
+
+        String query = "";
+        if(query_ != null)
+            query = query_;
+
         NoticeService service = new NoticeService();
-        List<Notice> list = service.getNoticeList();
+        List<Notice> list = service.getNoticeList(field, query, 1);
 
         request.setAttribute("list", list);
 
