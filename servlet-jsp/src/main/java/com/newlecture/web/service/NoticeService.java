2291f2e58114dbdf96a67af5dbae6cfe05592364
seohyoseok0408 <seohyoseok0408@gmail.com>
2024-07-11 09:10:07 +0900

    서블릿/jsp-admin페이지, service정의

--- servlet-jsp/src/main/java/com/newlecture/web/service/NoticeService.java
+++ servlet-jsp/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -8,6 +8,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NoticeService {
+    public int removeNoticeAll(int[] ids){
+        return 0;
+    }
+
+    public int pubNoticeAll(int[] ids){
+        return 0;
+    }
+    public int insertNotice(Notice notice){
+        return 0;
+    }
+
+    public int deleteNotice(int id){
+        return 0;
+    }
+
+    public int updateNotice(Notice notice){
+        return 0;
+    }
+
+    List<Notice> getNoticeNewestList() {
+        return null;
+    }
+
+
     public List<NoticeView> getNoticeList() {
         return getNoticeList("title", "", 1);
     }
