12f961fe554aa97f81f45088b437178c5d809647
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-10-06 17:03:57 +0900

    feat: SecurityConfig 사용

--- spring/blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ spring/blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -2,6 +2,7 @@ package com.cos.blog.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,22 +20,10 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping("api/user")
+	@PostMapping("auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController : save 호출됨");
-		user.setRole(RoleType.USER);
 		userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
-	
-	@PostMapping("api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
-		System.out.println("UserApiController : login 호출됨");
-		User principal = userService.로그인(user); // principal (접근주체)
-		
-		if (principal != null) { //로그인 시 세션 생성 
-			session.setAttribute("principal", principal);
-		}
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
