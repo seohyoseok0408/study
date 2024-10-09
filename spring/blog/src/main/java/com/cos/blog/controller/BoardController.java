32cba0a0f9b167a257d4ac33dd84bea8d3beaf68
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-10-09 14:03:15 +0900

    feat: 글목록 페이징

--- spring/blog/src/main/java/com/cos/blog/controller/BoardController.java
+++ spring/blog/src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,6 +1,10 @@
 package com.cos.blog.controller;
 
+import org.hibernate.query.SortDirection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +20,11 @@ public class BoardController {
 	BoardService boardService;
 
 	@GetMapping({"", "/"})
-	public String index(Model model) {
-		model.addAttribute("boards", boardService.글목록());
+	public String index(Model model, @PageableDefault(size =3, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
+		model.addAttribute("boards", boardService.글목록(pageable));
 		return "index";
 	}
+	
 	
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
