6ed0a52568f08ca6f07fd7c4e7695b973982c2b5
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-10-09 16:47:27 +0900

    feat: 글수정하기

--- spring/blog/src/main/java/com/cos/blog/service/BoardService.java
+++ spring/blog/src/main/java/com/cos/blog/service/BoardService.java
@@ -34,14 +34,24 @@ public class BoardService {
 			return new IllegalArgumentException("글 상세보기 실패: 아이디를 찾을 수 없습니다.");
 		});
 	}
-	
+
 	@Transactional(readOnly = true)
 	public Page<Board> 글목록(Pageable pageable) {
 		return boardRepository.findAll(pageable);
 	}
-	
+
 	@Transactional
 	public void 글삭제하기(int id) {
 		boardRepository.deleteById(id);
+	}
+
+	@Transactional
+	public void 글수정하기(int id, Board requestBoard) {
+		Board board = boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("글 찾기 실패: 아이디를 찾을 수 없습니다.");
+		}); // 영속화 완료
+		board.setTitle(requestBoard.getTitle());
+		board.setContent(requestBoard.getContent());
+		// 해당 함수 종료시(Serivce종료시) 트랜잭션 종료 -> 더티체킹 -> 자동업데이트. db flush
 	}
 }
--- spring/blog/src/main/resources/static/js/board.js
+++ spring/blog/src/main/resources/static/js/board.js
@@ -6,6 +6,9 @@ let index = {
 		$("#btn-delete").on("click", () => {
 			this.deleteById();
 		});
+		$("#btn-update").on("click", () => {
+			this.update();
+		});
 	},
 
 	save: function() {
@@ -28,14 +31,36 @@ let index = {
 	},
 
 	deleteById: function() {
-		var id = $("#id").text() 
-		
+		let id = $("#id").text()
+
 		$.ajax({
 			type: "DELETE",
-			url: "/api/board/"+id,
+			url: "/api/board/" + id,
 			dataType: "json",
 		}).done(function(resp) {
 			alert("삭제가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error))
+		});
+	},
+
+	update: function() {
+		let id = $("#id").val();
+
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val(),
+		};
+
+		$.ajax({
+			type: "PUT",
+			url: "/api/board/" + id,
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+		}).done(function(resp) {
+			alert("글수정 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error))
