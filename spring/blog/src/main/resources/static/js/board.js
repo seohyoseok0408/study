c093741522e002c89a1ddb2c3c8d59f70a907be2
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-10-09 14:43:02 +0900

    feat: 글삭제하기

--- spring/blog/src/main/resources/static/js/board.js
+++ spring/blog/src/main/resources/static/js/board.js
@@ -3,6 +3,9 @@ let index = {
 		$("#btn-save").on("click", () => {
 			this.save();
 		});
+		$("#btn-delete").on("click", () => {
+			this.deleteById();
+		});
 	},
 
 	save: function() {
@@ -13,11 +16,26 @@ let index = {
 		$.ajax({
 			type: "POST",
 			url: "/api/board",
-			data: JSON.stringify(data), 
+			data: JSON.stringify(data),
 			contentType: "application/json; charset=utf-8",
-			dataType: "json", 
+			dataType: "json",
 		}).done(function(resp) {
 			alert("글쓰기가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error))
+		});
+	},
+
+	deleteById: function() {
+		var id = $("#id").text() 
+		
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/"+id,
+			dataType: "json",
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error))
