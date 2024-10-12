08a6cac3571945ab16872c108426be7cd2d2aac6
Hyoseok Seo <seohyoseok0408@gmail.com>
2024-10-12 15:00:00 +0900

    feat: 회원수정

--- spring/blog/src/main/resources/static/js/user.js
+++ spring/blog/src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
 		$("#btn-save").on("click", () => { // 화살표함수 쓰는 이유는 this를 바인딩하기 위해
 			this.save();
 		});
+		$("#btn-update").on("click", () => { // 화살표함수 쓰는 이유는 this를 바인딩하기 위해
+					this.update();
+				});
 	},
 
 	save: function() {
@@ -31,6 +34,27 @@ let index = {
 			alert(JSON.stringify(error))
 		});
 	},
+	
+	update: function() {
+			let data = {
+				id: $("#id").val(),
+				password: $("#password").val(),
+				email: $("#email").val()
+			};
+
+			$.ajax({
+				type: "PUT",
+				url: "/user",
+				data: JSON.stringify(data), 
+				contentType: "application/json; charset=utf-8", 
+				dataType: "json",   
+			}).done(function(resp) {
+				alert("회원수정이 완료되었습니다.");
+				location.href = "/";
+			}).fail(function(error) {
+				alert(JSON.stringify(error))
+			});
+		},
 }
 
 index.init();
